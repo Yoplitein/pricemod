08a8035296db2bdf996e502890483f67d5070221
Steven Dwy <yoplitein+git@gmail.com>
2018-09-14 19:43:35 -0700

    Load price mods from config

--- src/main/java/net/yoplitein/wurm/pricemod/PriceMod.java
+++ src/main/java/net/yoplitein/wurm/pricemod/PriceMod.java
@@ -1,16 +1,37 @@
 package net.yoplitein.wurm.pricemod;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Properties;
 
 import com.wurmonline.server.items.ItemTemplate;
 import com.wurmonline.server.items.ItemTemplateFactory;
 import org.gotti.wurmunlimited.modloader.classhooks.HookException;
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
 import org.gotti.wurmunlimited.modloader.interfaces.ItemTemplatesCreatedListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
-public class PriceMod implements WurmServerMod, ItemTemplatesCreatedListener
+public class PriceMod implements WurmServerMod, ItemTemplatesCreatedListener, Configurable
 {
     Field valueField;
+    HashMap<Integer, Integer> priceMods = new HashMap<>();
+    
+    @Override
+    public void configure(Properties properties)
+    {
+        for(String key: properties.stringPropertyNames())
+        {
+            if(!key.startsWith("price"))
+                continue;
+            
+            String value = properties.getProperty(key);
+            String[] bits = value.split(",");
+            int id = Integer.parseInt(bits[0]);
+            int price = Integer.parseInt(bits[1]);
+            
+            priceMods.put(id, price);
+        }
+    }
     
     @Override
     public void onItemTemplatesCreated()
@@ -26,7 +47,17 @@ public class PriceMod implements WurmServerMod, ItemTemplatesCreatedListener
             throw new HookException(err);
         }
         
-        setItemPrice(601, 2500); //shaker orb
+        for(int id: priceMods.keySet())
+        {
+            try
+            {
+                setItemPrice(id, priceMods.get(id));
+            }
+            catch(HookException err)
+            {
+                throw new RuntimeException(String.format("Failed to set price of item %d", id), err);
+            }
+        }
     }
     
     private void setItemPrice(int id, int newPrice)
